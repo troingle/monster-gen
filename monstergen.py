67d153e082f1d1f0682b94b47ae57917b725f5fd
troingle <109359272+troingle@users.noreply.github.com>
2023-07-27 15:07:56 +0300

    Revamped output look

--- monstergen.py
+++ monstergen.py
@@ -1,5 +1,6 @@
 import json
 import random
+import os
 
 monsters = []
 original_list = []
@@ -9,27 +10,28 @@ with open("monsters.json") as file:
 
 for item in data:
 	nimi = str(item['nimi'])
-	kuvaus = str(item['kuvaus'])
+	kuvaus = str(item['monologi'])
 
-	monster = f"{nimi}: {kuvaus}"
+	monster = f"========{nimi.upper()}========\n\n{kuvaus}"
 	
 	monsters.append(monster)
 
 
 while True:
 	while True:
+	    
 		if len(monsters) == 0:
 			print("Sekoitetaan pakkaa...")
 			print("")
 			for item in data:
 				nimi = str(item['nimi'])
-				kuvaus = str(item['kuvaus'])
+				kuvaus = str(item['monologi'])
 
-				monster = f"{nimi}: {kuvaus}"
+				monster = f"========{nimi.upper()}========\n\n{kuvaus}"
 				
 				monsters.append(monster)
 		chosen = random.choice(monsters)
 		print(chosen)
 		monsters.remove(chosen)
 		input()
-	
+		os.system('clear')
